d8be6af4df97402bc2a43a2bdefc2172d720578d
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 11:48:51 +0100

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    acct = Account("Arto's account", 100)
+    acct.deposit(20)
+    print(acct.balance)
 
+# Don't edit below this line - this setup is required for testingss 
 if __name__ == '__main__':
+    from account import Account
     main()
+else:
+    from src.account import Account
